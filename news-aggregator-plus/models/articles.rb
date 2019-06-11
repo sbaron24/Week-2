8a9a710da54450c3b49a606cf9b8f42a6a27285d
Sean Baron <seanbaron27@gmail.com>
2019-06-11 18:30:20 -0400

    Removed binding.pry

--- news-aggregator-plus/models/articles.rb
+++ news-aggregator-plus/models/articles.rb
@@ -12,19 +12,10 @@ class Article
 #should we put all of this in server file instead?
   def self.read_csv(csv_file)
     article_array = []
-    # csv_objects = CSV.readlines('articles.csv', headers: true)
-    #
-    # csv_objects.each do |csv_object|
-    #   article_array << Article.new(csv_object[:title],csv_object[:description],csv_object[:url])
-    # end
-    # binding.pry
     CSV.foreach(csv_file, headers:true) do |row|
-      binding.pry
       hash_row = row.to_h
       article_array << Article.new(hash_row['title'],hash_row['description'],hash_row['url'])
     end
-#    binding.pry
-
-    return article_array
+    article_array
   end
 end
